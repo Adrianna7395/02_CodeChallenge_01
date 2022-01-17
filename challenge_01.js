d147922c89a405e6be4131d8a17c12850d1210d4
Adriana <hrl.davidson@gmail.com>
2022-01-17 12:59:26 +0100

    lösung

--- challenge_01.js
+++ challenge_01.js
@@ -8,4 +8,16 @@ Berechnen Sie die Summe der beiden Zahlen.
 Geben Sie die Summe der Zahlen in die Konsole aus:
 "Die Summe der Zahlen ist: summe"
 */
+// Deklaration
+let ersteZahl;
+let zweiteZahl;
+let summe;
+// Wertzuweisung
+ersteZahl = Number (prompt());
+zweiteZahl = Number (prompt());
 
+// Berechnung
+summe = ersteZahl + zweiteZahl;
+
+// Ausgabe
+console.log ("die summe ist" + " " + summe);
